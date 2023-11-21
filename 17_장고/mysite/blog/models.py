2f7aa6043bb2a519c55f1c9c0372b27595a4a7ae
WooyoungPark <wooyoung9654@gmail.com>
2023-11-21 22:05:31 +0900

    admin 앱에서 데이터 조작
    - Views에서 서비스처리 뷰함수 구현
    - 모델 관계 설정
    - admin 앱에서 내가 구현한 모델 기반 Form 사용

--- 17_장고/mysite/blog/models.py
+++ 17_장고/mysite/blog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=250)
+    body = models.TextField()
+    
+    # (디폴트) 앱명_모델명 소문자
+    
+    def __str__(self):
+        return self.title
+
+# 모델 관계 설정
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)  # Post 모델과 1:N 관계 설정
+    author = models.CharField(max_length=20)
+    message = models.TextField()
+    created = models.DateField(auto_now_add=True)  # 최초 저장 시점의 시간 저장
+    updated = models.DateTimeField(auto_now=True)  # 객체 저장 시점의 시간 저장
+    
--- 17_장고/mysite/blog/urls.py
+++ 17_장고/mysite/blog/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.list),   # 웹브라우저에서 http://localhost:8000/blog/ 요청할 때
+    path('<int:id>/', views.detail), 
     path('test1/', views.test1),
     path('test2/<int:no>/', views.test2),
 ]
