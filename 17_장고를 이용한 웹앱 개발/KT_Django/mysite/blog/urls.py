4bd6d53b2ebca626852283233522d4b848221013
WooyoungPark <wooyoung9654@gmail.com>
2023-11-22 15:18:28 +0900

    장고 수업 private

--- 17_장고를 이용한 웹앱 개발/KT_Django/mysite/blog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.list),   # 웹브라우저에서 http://localhost:8000/blog/ 요청할 때
-    path('<int:id>/', views.detail), 
-    path('test1/', views.test1),
-    path('test2/<int:no>/', views.test2),
-]
